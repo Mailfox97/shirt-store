f9a2ce2271d06a46be2472e6bec32da029109ef9
ngan <nttn.love.waffle@gmail.com>
2021-11-22 16:39:56 +0700

    seo

--- shirts/admin.py
+++ shirts/admin.py
@@ -1,14 +1,48 @@
+import csv
+
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.http import HttpResponse
+
 from .models import *
 
+
+User = get_user_model()
+
+
 # Register your models here.
+def export_csv_email(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="users.csv"'  # your filename
+
+    writer = csv.writer(response)
+    writer.writerow(['Email Address'])
+
+    users = queryset.values_list('email')
+
+    for user in users:
+        writer.writerow(user)
+
+    return response
+
+
+export_csv_email.short_description = "Export email"
+
+
+class UserAdmin(UserAdmin):
+    '''
+    Registers the action in your model admin
+    '''
+    actions = [export_csv_email]
+
 
 class SizeVariantConfig(admin.TabularInline):
     model = Sizevariant
 
-class TshirtConfig(admin.ModelAdmin):
-    inlines = [SizeVariantConfig]    
 
+class TshirtConfig(admin.ModelAdmin):
+    inlines = [SizeVariantConfig]
 
 
 admin.site.register(Tshirt, TshirtConfig)
@@ -22,3 +56,5 @@ admin.site.register(color)
 # admin.site.register(Payment)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
